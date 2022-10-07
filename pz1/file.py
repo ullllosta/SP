bb38cc7736c8b2bde8d821178e96c177375f603c
lizay <lizayakushova172@gmail.com>
2022-10-07 23:42:48 +0300

    1 практическая работа

---
+++ pz1/file.py
@@ -0,0 +1,17 @@
+selects = str(input('Нажмите Enter для начала заполнения формы'))
+name = str("Имя:"+input('Введите Имя: '))
+fname = str("Фамилия:"+input('Введите Фамилию: '))
+year = str("Возраст:"+input('Введите возраст: '))
+card = str('Номер карты:'+input('Введите номер карты: '))
+validity = str('Срок действия карты:'+input('Введите срок действия карты: '))
+code = str('Код:'+input('Введите код на обратной стороне: '))
+file = [name, fname, year, card, validity, code]
+file_total = open("file1.txt", "a")
+
+for item in file:
+    file_total.write("%s;\xa0\xa0" % item)
+file_total.write('\n')
+file_total.close()
+file_total = open("file1.txt")
+total = file_total.read()
+print(total)
